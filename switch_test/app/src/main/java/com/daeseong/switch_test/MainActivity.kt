ba9f1213655559373906f04ccaf93bc97d380cc1
ImDaeseong <cs93059@gmail.com>
2021-02-06 13:52:40 +0900

    DaeseongKotlin

---
+++ switch_test/app/src/main/java/com/daeseong/switch_test/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.daeseong.switch_test
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val tag: String = MainActivity::class.java.simpleName;
+
+    private var switch1 : Switch? = null;
+    private var switch2: Switch? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        switch1 = findViewById<Switch>(R.id.switch1)
+        switch1!!.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+
+                Log.e(tag , "isChecked");
+
+            } else {
+
+                Log.e(tag , "isUnChecked");
+            }
+        }
+
+        switch2 = findViewById<Switch>(R.id.switch2)
+        switch2!!.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+
+                Log.e(tag , "isChecked");
+
+            } else {
+
+                Log.e(tag , "isUnChecked");
+            }
+        }
+
+    }
+}
