cde6bb349ef142fc6dddb88b705ecaf36215d917
ImDaeseong <cs93059@gmail.com>
2023-01-13 18:48:01 +0900

    DaeseongKotlin

--- edittext_test/app/src/main/java/com/daeseong/edittext_test/MainActivity.kt
+++ edittext_test/app/src/main/java/com/daeseong/edittext_test/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.daeseong.edittext_test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var et1: EditText? = null
+    private var et2: EditText? = null
+    private var et3: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        et1 = findViewById<View>(R.id.et1) as EditText
+        //et1!!.background.setColorFilter(Color.parseColor("#EDEDED"), PorterDuff.Mode.SRC_IN)
+        et1!!.setText("")
+        et1!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+        et2 = findViewById<View>(R.id.et2) as EditText
+        //et2!!.background.setColorFilter(Color.parseColor("#EDEDED"), PorterDuff.Mode.SRC_IN)
+        et2!!.setText("")
+        et2!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+        et3 = findViewById<View>(R.id.et3) as EditText
+        //et3!!.background.setColorFilter(Color.parseColor("#EDEDED"), PorterDuff.Mode.SRC_IN)
+        et3!!.setText("")
+        et3!!.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+            }
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
     }
 }
