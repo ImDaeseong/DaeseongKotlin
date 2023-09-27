dc7d4abda3cbdf496bdc3f1665616e587b360c69
ImDaeseong <cs93059@gmail.com>
2023-09-27 14:37:28 +0900

    DaeseongKotlin

--- timer_test/app/src/main/java/com/daeseong/timer_test/MainActivity.kt
+++ timer_test/app/src/main/java/com/daeseong/timer_test/MainActivity.kt
@@ -1,86 +1,71 @@
 package com.daeseong.timer_test
 
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-
 class MainActivity : AppCompatActivity() {
 
     private val tag: String = MainActivity::class.java.simpleName
 
-    private var tv1: TextView? = null
-    private var btn1: Button? = null
+    private lateinit var tv1: TextView
+    private lateinit var btn1: Button
 
     private var timer: Timer? = null
-
-    private fun closeTimer() {
-        try {
-            if (timer != null) {
-                timer!!.cancel()
-                timer = null
-            }
-        } catch (e: Exception) {
-        }
-    }
-
-    private fun startTimer() {
-
-        closeTimer()
-
-        timer = Timer()
-        timer!!.schedule(object : TimerTask() {
-
-            var nTimeCount = 0
-            override fun run() {
-                try {
-
-                    nTimeCount++
-                    runOnUiThread {
-                        val sMsg = String.format("%d", nTimeCount)
-                        tv1!!.text = sMsg
-                    }
-
-                    if (nTimeCount > 180) { //3분까지만 체크
-                        closeTimer()
-                        runOnUiThread {
-                            Toast.makeText(this@MainActivity, "Toast test", Toast.LENGTH_SHORT).show()
-                            tv1!!.text = "완료"
-                        }
-                    }
-                } catch (e: java.lang.Exception) {
-                }
-            }
-
-        }, 0, 1000)
-    }
+    private var nTimeCount = 0
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv1 = findViewById<TextView>(R.id.tv1)
+        tv1 = findViewById(R.id.tv1)
+        btn1 = findViewById(R.id.btn1)
 
-        btn1 = findViewById<Button>(R.id.btn1)
-        btn1!!.setOnClickListener(View.OnClickListener {
-            startTimer();
-        })
-
+        btn1.setOnClickListener {
+            startTimer()
+        }
     }
 
     override fun onPause() {
         super.onPause()
-
-        closeTimer();
+        closeTimer()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        closeTimer()
+    }
 
-        closeTimer();
+    private fun startTimer() {
+        closeTimer()
+
+        timer = Timer()
+        timer?.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                nTimeCount++
+                handler.post {
+                    tv1.text = nTimeCount.toString()
+                }
+
+                if (nTimeCount > 180) {
+                    closeTimer()
+                    handler.post {
+                        Toast.makeText(this@MainActivity, "Toast test", Toast.LENGTH_SHORT).show()
+                        tv1.text = "완료"
+                    }
+                }
+            }
+        }, 0, 1000)
+    }
+
+    private fun closeTimer() {
+        timer?.cancel()
+        timer = null
     }
 }
