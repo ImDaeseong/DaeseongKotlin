00f42652cf123fb776318edee1563f2d3febb8d8
ImDaeseong <cs93059@gmail.com>
2023-10-04 16:11:20 +0900

    DaeseongKotlin

--- switch_test/app/src/main/java/com/daeseong/switch_test/MainActivity.kt
+++ switch_test/app/src/main/java/com/daeseong/switch_test/MainActivity.kt
@@ -5,41 +5,33 @@ import android.util.Log
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
 
     private val tag: String = MainActivity::class.java.simpleName
 
-    private var switch1 : Switch? = null
-    private var switch2: Switch? = null
+    private lateinit var switch1: Switch
+    private lateinit var switch2: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        switch1 = findViewById<Switch>(R.id.switch1)
-        switch1!!.setOnCheckedChangeListener { _, isChecked ->
+        switch1 = findViewById(R.id.switch1)
+        switch1.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-
-                Log.e(tag , "isChecked")
-
+                Log.e(tag, "isChecked")
             } else {
-
-                Log.e(tag , "isUnChecked")
+                Log.e(tag, "isUnChecked")
             }
         }
 
-        switch2 = findViewById<Switch>(R.id.switch2)
-        switch2!!.setOnCheckedChangeListener { _, isChecked ->
+        switch2 = findViewById(R.id.switch2)
+        switch2.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-
-                Log.e(tag , "isChecked")
-
+                Log.e(tag, "isChecked")
             } else {
-
-                Log.e(tag , "isUnChecked")
+                Log.e(tag, "isUnChecked")
             }
         }
-
     }
 }
