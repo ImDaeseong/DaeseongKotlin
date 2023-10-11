d8863b58b6d771b87eb1e9695c8947d714675cef
ImDaeseong <cs93059@gmail.com>
2023-10-11 16:59:26 +0900

    DaeseongKotlin

--- BLUETOOTH_test/app/src/main/java/com/daeseong/bluetooth_test/MainActivity.kt
+++ BLUETOOTH_test/app/src/main/java/com/daeseong/bluetooth_test/MainActivity.kt
@@ -10,78 +10,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val TAG = MainActivity::class.java.simpleName
 
-    private var button1 : Button? = null
-    private var button2 : Button? = null
-    private var button3 : Button? = null
-    private var button4 : Button? = null
-    private var button5 : Button? = null
-    private var button6 : Button? = null
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+    private lateinit var button5: Button
+    private lateinit var button6: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1 = findViewById<Button>(R.id.button1)
-        button1!!.setOnClickListener(this)
+        button1 = findViewById(R.id.button1)
+        button1.setOnClickListener(this)
 
-        button2 = findViewById<Button>(R.id.button2)
-        button2!!.setOnClickListener(this)
+        button2 = findViewById(R.id.button2)
+        button2.setOnClickListener(this)
 
-        button3 = findViewById<Button>(R.id.button3)
-        button3!!.setOnClickListener(this)
+        button3 = findViewById(R.id.button3)
+        button3.setOnClickListener(this)
 
-        button4 = findViewById<Button>(R.id.button4)
-        button4!!.setOnClickListener(this)
+        button4 = findViewById(R.id.button4)
+        button4.setOnClickListener(this)
 
-        button5 = findViewById<Button>(R.id.button5)
-        button5!!.setOnClickListener(this)
+        button5 = findViewById(R.id.button5)
+        button5.setOnClickListener(this)
 
-        button6 = findViewById<Button>(R.id.button6)
-        button6!!.setOnClickListener(this)
+        button6 = findViewById(R.id.button6)
+        button6.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-
-        if (v != null) {
-
-            when (v.id) {
-
-                R.id.button1 -> {
-
-                    val intent = Intent(this, MainActivity1::class.java)
-                    startActivity(intent)
-                }
-
-                R.id.button2 -> {
-
-                    val intent = Intent(this, MainActivity2::class.java)
-                    startActivity(intent)
-                }
-
-                R.id.button3 -> {
-
-                    val intent = Intent(this, MainActivity3::class.java)
-                    startActivity(intent)
-                }
-
-                R.id.button4 -> {
-
-                    val intent = Intent(this, MainActivity4::class.java)
-                    startActivity(intent)
-                }
-
-                R.id.button5 -> {
-
-                    val intent = Intent(this, MainActivity5::class.java)
-                    startActivity(intent)
-                }
-
-                R.id.button6 -> {
-
-                    val intent = Intent(this, MainActivity6::class.java)
-                    startActivity(intent)
-                }
-            }
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button1 -> startActivity(Intent(this, MainActivity1::class.java))
+            R.id.button2 -> startActivity(Intent(this, MainActivity2::class.java))
+            R.id.button3 -> startActivity(Intent(this, MainActivity3::class.java))
+            R.id.button4 -> startActivity(Intent(this, MainActivity4::class.java))
+            R.id.button5 -> startActivity(Intent(this, MainActivity5::class.java))
+            R.id.button6 -> startActivity(Intent(this, MainActivity6::class.java))
         }
     }
 }
