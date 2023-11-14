817ef77164777ee887757d5582c08dc9b5a5ae2e
ImDaeseong <cs93059@gmail.com>
2023-11-14 15:21:51 +0900

    DaeseongKotlin

--- keepscreenon_test/app/src/main/java/com/daeseong/keepscreenon_test/MainActivity.kt
+++ keepscreenon_test/app/src/main/java/com/daeseong/keepscreenon_test/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.daeseong.keepscreenon_test
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -9,21 +10,25 @@ class MainActivity : AppCompatActivity() {
 
     private val tag = MainActivity::class.java.simpleName
 
-    private var btn1: Button? = null
-    private var btn2: Button? = null
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn1 = findViewById<Button>(R.id.btn1)
-        btn1!!.setOnClickListener(View.OnClickListener {
-            ScreenLockutil().Lock(this)
+        btn1 = findViewById(R.id.btn1)
+        btn1.setOnClickListener(View.OnClickListener {
+
+            Log.e(tag, "화면을 계속 켜두고 비활성화되지 않도록")
+            ScreenLockUtil().lock(this)
         })
 
-        btn2 = findViewById<Button>(R.id.btn2)
-        btn2!!.setOnClickListener(View.OnClickListener {
-            ScreenLockutil().Unlock(this)
+        btn2 = findViewById(R.id.btn2)
+        btn2.setOnClickListener(View.OnClickListener {
+
+            Log.e(tag, "화면 잠금을 해제하여 화면이 기기의 일반적인 동작")
+            ScreenLockUtil().unlock(this)
         })
     }
 }
--- keepscreenon_test/app/src/main/java/com/daeseong/keepscreenon_test/ScreenLockutil.kt
+++ keepscreenon_test/app/src/main/java/com/daeseong/keepscreenon_test/ScreenLockutil.kt
@@ -3,18 +3,17 @@ package com.daeseong.keepscreenon_test
 import android.app.Activity
 import android.view.WindowManager
 
+class ScreenLockUtil {
 
-class ScreenLockutil {
-
-    private val tag = ScreenLockutil::class.java.simpleName
+    private val tag = ScreenLockUtil::class.java.simpleName
 
     //화면이 꺼지지 않게 고정
-    fun Lock(activity: Activity) {
+    fun lock(activity: Activity) {
         activity.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
     //해제
-    fun Unlock(activity: Activity) {
+    fun unlock(activity: Activity) {
         activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
