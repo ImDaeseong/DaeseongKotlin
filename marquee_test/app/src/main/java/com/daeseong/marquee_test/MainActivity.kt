1f3af37ae621e29b305daa37bf5a73ffe96616c3
ImDaeseong <cs93059@gmail.com>
2023-11-14 15:13:59 +0900

    DaeseongKotlin

--- marquee_test/app/src/main/java/com/daeseong/marquee_test/MainActivity.kt
+++ marquee_test/app/src/main/java/com/daeseong/marquee_test/MainActivity.kt
@@ -3,54 +3,38 @@ package com.daeseong.marquee_test
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
-
 
 class MainActivity : AppCompatActivity() {
 
     private val tag: String = MainActivity::class.java.simpleName
 
     private var tv1: TextView? = null
-
-    private var task: MarqueeTask? = null
-
-    private var timer: Timer? = null
-
-
-    private fun closeTimer() {
-
-        if (timer != null) {
-            timer!!.cancel()
-            timer = null
-        }
-    }
-
-    private fun startTimer() {
-
-        closeTimer()
-        task = MarqueeTask(tv1!!)
-        timer = Timer()
-        timer!!.schedule(task, 0, 10000)
-    }
+    private var marqueeTask: MarqueeTask? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv1  = findViewById<TextView>(R.id.tv1 )
+        tv1  = findViewById(R.id.tv1 )
 
-        startTimer()
+        marqueeTask = MarqueeTask(tv1!!)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        marqueeTask?.run()
     }
 
     override fun onPause() {
         super.onPause()
 
-        closeTimer()
+        marqueeTask?.stop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
 
-        closeTimer()
+        marqueeTask?.stop()
     }
 }
--- marquee_test/app/src/main/java/com/daeseong/marquee_test/MarqueeTask.kt
+++ marquee_test/app/src/main/java/com/daeseong/marquee_test/MarqueeTask.kt
@@ -1,53 +1,47 @@
 package com.daeseong.marquee_test
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.os.Handler
-import android.view.View
+import android.os.Looper
 import android.widget.TextView
-import java.util.*
 
-class MarqueeTask(tv: TextView) : TimerTask() {
+class MarqueeTask(private val tv: TextView) : Runnable {
 
-    private var handler: Handler? = null
-    private var tv: TextView? = null
     private var bFlag = false
-
-    init {
-        this.tv = tv
-        handler = Handler()
-    }
+    private var animator: ValueAnimator? = null
+    private val handler: Handler = Handler(Looper.getMainLooper())
 
     override fun run() {
-
-        handler!!.post {
-            marquee(tv, bFlag)
-        }
+        marquee(tv, bFlag)
+        handler.postDelayed(this, 5000)
     }
 
-    private fun marquee(view: View?, bFlag: Boolean) {
+    private fun marquee(view: TextView, bFlag: Boolean) {
+        val initialTranslationX = 0f
+        val endTranslationX = if (bFlag) -1000f else 1000f
 
-        if (bFlag) {
+        animator?.cancel()
 
-            val animator1 = ObjectAnimator.ofFloat(view, "translationX", 0f, -1000f)
-            animator1.duration = 500
-            val animator2 = ObjectAnimator.ofFloat(view, "translationX", 1000f, 0f)
-            animator2.duration = 500
-            val animatorSet = AnimatorSet()
-            animatorSet.playSequentially(animator1, animator2)
-            animatorSet.start()
-
-        } else {
-
-            val animator1 = ObjectAnimator.ofFloat(view, "translationX", 0f, 1000f)
-            animator1.duration = 500
-            val animator2 = ObjectAnimator.ofFloat(view, "translationX", -1000f, 0f)
-            animator2.duration = 500
-            val animatorSet = AnimatorSet()
-            animatorSet.playSequentially(animator1, animator2)
-            animatorSet.start()
+        animator = ValueAnimator.ofFloat(initialTranslationX, endTranslationX)
+        animator?.duration = 1000
+        animator?.addUpdateListener { valueAnimator ->
+            view.translationX = valueAnimator.animatedValue as Float
         }
 
-        this.bFlag = !bFlag
+        animator?.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                view.translationX = initialTranslationX
+                this@MarqueeTask.bFlag = !bFlag
+            }
+        })
+
+        animator?.start()
+    }
+
+    fun stop() {
+        handler.removeCallbacks(this)
+        animator?.cancel()
     }
 }
