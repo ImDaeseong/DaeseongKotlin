01f51172cef1062b8ce1c066d5c0e6d3a5330826
ImDaeseong <cs93059@gmail.com>
2024-03-07 15:36:31 +0900

    DaeseongKotlin

---
+++ CoordinatorLayout_test/app/src/main/java/com/daeseong/coordinatorlayout_test/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.daeseong.coordinatorlayout_test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var button4: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+        button4 = findViewById(R.id.button4)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button1 -> startActivity(Intent(this, coordinatorlayout1Activity::class.java))
+            R.id.button2 -> startActivity(Intent(this, coordinatorlayout2Activity::class.java))
+            R.id.button3 -> startActivity(Intent(this, coordinatorlayout3Activity::class.java))
+            R.id.button4 -> startActivity(Intent(this, coordinatorlayout4Activity::class.java))
+        }
+    }
+}
