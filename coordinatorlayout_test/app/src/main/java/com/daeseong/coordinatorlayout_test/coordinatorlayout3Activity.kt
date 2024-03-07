40cec4fe987266d4186fc470808e4c56e257ca8c
ImDaeseong <cs93059@gmail.com>
2024-03-07 14:37:11 +0900

    DaeseongKotlin

--- coordinatorlayout_test/app/src/main/java/com/daeseong/coordinatorlayout_test/coordinatorlayout3Activity.kt
+++ coordinatorlayout_test/app/src/main/java/com/daeseong/coordinatorlayout_test/coordinatorlayout3Activity.kt
@@ -2,16 +2,30 @@ package com.daeseong.coordinatorlayout_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.google.android.material.appbar.AppBarLayout
 
 class coordinatorlayout3Activity : AppCompatActivity() {
 
+    private val tag = coordinatorlayout3Activity::class.java.simpleName
+
+    private lateinit var aB1: AppBarLayout
     private lateinit var web1: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinatorlayout3)
+
+        aB1 = findViewById(R.id.aB1)
+        aB1.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (verticalOffset == 0) {
+                Log.e(tag, "툴바가 완전히 펼쳐진 상태")
+            } else {
+                Log.e(tag, "툴바가 축소된 상태")
+            }
+        }
 
         web1 = findViewById(R.id.web1)
         web1.settings.javaScriptEnabled = true
