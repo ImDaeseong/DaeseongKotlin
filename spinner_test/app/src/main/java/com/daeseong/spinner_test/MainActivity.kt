d7351242b96f955544d8d5848b5d4aa215d20d3d
ImDaeseong <cs93059@gmail.com>
2024-03-18 18:52:54 +0900

    DaeseongKotlin

--- spinner_test/app/src/main/java/com/daeseong/spinner_test/MainActivity.kt
+++ spinner_test/app/src/main/java/com/daeseong/spinner_test/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var button2: Button
     private lateinit var button3: Button
     private lateinit var button4: Button
+    private lateinit var button5: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +22,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button2 = findViewById(R.id.button2)
         button3 = findViewById(R.id.button3)
         button4 = findViewById(R.id.button4)
+        button5 = findViewById(R.id.button5)
 
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -35,6 +38,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 R.id.button2 -> Intent(this, Spinner2Activity::class.java)
                 R.id.button3 -> Intent(this, Spinner3Activity::class.java)
                 R.id.button4 -> Intent(this, Spinner4Activity::class.java)
+                R.id.button5 -> Intent(this, Spinner5Activity::class.java)
                 else -> null
             }
             intent?.let { startActivity(it) }
